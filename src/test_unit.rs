95d574747ccf954cf06aef4b6df03d9fca973a03
Mihai Galos <mihaigalos@gmail.com>
2019-10-20 22:07:04 +0200

    Use relative paths

--- src/test_unit.rs
+++ src/test_unit.rs
@@ -8,21 +8,21 @@ mod tests {
         let dot_generator = DotGenerator {};
         let args = [
             "zuulgraph",
-            "/home/mihai/git/zuulgraph/test/yaml/demo_file1.yaml",
-            "/home/mihai/git/zuulgraph/test/yaml/demo_file2.yaml",
+            "test/yaml/demo_file1.yaml",
+            "test/yaml/demo_file2.yaml",
         ]
         .iter()
         .map(|&s| s.into())
         .collect();
         let expected = concat!(
             "digraph D {\n",
-            "  JobB_file1 -> A_file2\n",
+            "  \"JobB_file1\" -> \"A_file2\"\n",
             "  \"JobB_file1\" [style=filled, fillcolor=\"\"]\n",
-            "  JobC_file1 -> JobB_file1\n",
+            "  \"JobC_file1\" -> \"JobB_file1\"\n",
             "  \"JobC_file1\" [style=filled, fillcolor=\"\"]\n",
-            "  JobD_file1 -> JobB_file1\n",
+            "  \"JobD_file1\" -> \"JobB_file1\"\n",
             "  \"JobD_file1\" [style=filled, fillcolor=\"\"]\n",
-            "  A_file2 -> JobA_file1\n",
+            "  \"A_file2\" -> \"JobA_file1\"\n",
             "  \"A_file2\" [style=filled, fillcolor=\"\"]\n",
             "}\n"
         );
